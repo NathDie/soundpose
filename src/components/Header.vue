e37b6e429758ee179275daf8f6bb2572cb7c9789
NathDie <nath59310@gmail.com>
2026-09-18 17:40:22 +0200

    feat(header): (WIP) responsive style

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+</script>
+
+<template>
+  <header>
+    <img src="/logo.png" alt="Logo" />
+    <div class="content">
+      <h1>SoundPose<span>Quiz musical - 2 équipes</span></h1>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  img {
+    width: 5rem;
+  }
+
+  h1 {
+    font-weight: 600;
+    font-size: 1.75rem;
+    line-height: 1;
+
+    span {
+      font-weight: 400;
+      display: block;
+      font-size: 0.45em;
+      font-family: Roboto, sans-serif;
+      margin-top: 0.35rem;
+    }
+  }
+}
+</style>
